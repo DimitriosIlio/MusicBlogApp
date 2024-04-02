730373b0c9b5222566ad7edd5d7a97d32642db67
Dimitrios Iliogamvros <dimitrioslio@gmail.com>
2024-04-02 13:52:37 +0200

    new commit

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,1 +1,22 @@
 import React, { useState } from "react";
+import "./form.css";
+import axios from "axios";
+
+function Register () {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const register = async (event) => {
+        event.preventDefault();
+        try {
+            let user = { username, email, password };
+            let res = await axios.post("http://localhost:8000/register", user);
+            console.log(res.data);
+        } catch (error) {}
+    };
+ 
+    return (
+        <div className="form-container"> 
+        <h1>SignUp</h1>
+    )
