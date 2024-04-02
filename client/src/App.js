58cedd4cb504449090f0cdf34001f99d66ec23a8
Dimitrios Iliogamvros <dimitrioslio@gmail.com>
2024-04-02 14:47:10 +0200

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blogs from "./components/Blogs";
+import BlogForm from "./components/BlogForm";
+
+
 
 function App() {
   return (
-    <div className="App"> 
-     <Login /> 
-     <Register />
-    </div>
+    <BrowserRouter> 
+      <Routes> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/" element={<Blogs />} />
+        <Route path="/newBlog" element={<BlogForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 } 
 
